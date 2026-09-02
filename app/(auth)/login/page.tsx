5e51375aeb1c4491169648fc5f7858556c8ec765
Will <will00987654@gmail.com>
2026-09-02 09:21:31 -0300

    feat: implement login page and global theme styles

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -73,9 +73,9 @@ export default function LoginPage() {
               </div>
             </div>
           </CardContent>
-          {/* 
           <CardFooter className="flex flex-col gap-4 pt-4">
             <Button
+              type="submit"
               className="w-full"
               disabled={isLoading}
             >
@@ -93,7 +93,7 @@ export default function LoginPage() {
                 Fale com o suporte
               </button>
             </p>
-          </CardFooter> */}
+          </CardFooter>
         </form>
       </Card>
     </div>
